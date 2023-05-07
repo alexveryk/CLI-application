ffdc3e14b18d15707c437b06dc688b5a48f07639
alexveryk <veryk.av@gmail.com>
2023-05-07 23:49:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Change file");
+const contactService = require("./contacts");
+require("yargs").argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      break;
+
+    case "add":
+      break;
+
+    case "remove":
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction({ action: "list" });
