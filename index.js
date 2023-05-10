7731aa39f12de23efabf3a75c412b424ebc0de73
alexveryk <veryk.av@gmail.com>
2023-05-11 00:32:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactService.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
 
     case "get":
